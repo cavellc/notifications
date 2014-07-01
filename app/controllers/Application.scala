5fe3f8406f8f760934a51c6c9f77c92f31c3dd0d
Pawel Raszewski <praszewski@gilt.com>
2014-07-01 15:38:30 +0100

    show 10 most recent requests as json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,29 @@
 package controllers
 
+import java.util.Date
+
 import play.api.Logger
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 
 object Application extends Controller {
 
+  val buffer = new scala.collection.mutable.Queue[JsValue]
+  val MAX_BUF_SIZE = 10
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+//    Ok(views.html.index(buffer.mkString(",")))
+    Ok(Json.arr(buffer))
   }
+
+
 
   def createRecord = Action(parse.json) { request =>
     Logger.info(s"Received Notification: " + Json.prettyPrint(request.body))
+
+    buffer += request.body
+    if(buffer.size > MAX_BUF_SIZE) buffer.dequeue()
+    
     NoContent
   }
 }
